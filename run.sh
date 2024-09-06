c8bf3fb660f31e35ba5f8b7db8458c02e79f0ae7
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-09-07 04:43:26 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,14 +10,9 @@ export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=9001
 flask run &
 
-# Start app-3.py on port 9002
-export FLASK_APP=app-3.py
+Start alpha-service-2.py on port 9002
+export FLASK_APP=alpha-service-2.py
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=9002
 flask run &
 
-# Start geo.py on port 9003
-export FLASK_APP=geo.py
-export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=9003
-flask run &
