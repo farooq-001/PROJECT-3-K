0124ebe5bb48f9915d8e6dc675ac1a7ff4569763
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-09-07 04:38:09 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 source /root/PROJECT-3-K/myenv/bin/activate
 
 # Start alpha-service-2.py on port 9001
-export FLASK_APP=alpha-service-2.py
-#export FLASK_APP=app-4-pro.py
+#export FLASK_APP=alpha-service-2.py
+export FLASK_APP=app-4-pro.py
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=9001
 flask run &
